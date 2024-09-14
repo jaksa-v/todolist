df3326e988f4e8af29b514bc12f4796654a24610
Jaksa Vlahovic <jaksa.vlahovic1@gmail.com>
2024-09-14 16:33:54 +0200

    Install theme-change and implement theme switching

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,9 +1,13 @@
 <script setup>
 import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import ThemeSwitcher from "@/Components/ThemeSwitcher.vue";
 import { Link } from "@inertiajs/vue3";
 </script>
 
 <template>
+    <div class="absolute top-4 right-4">
+        <ThemeSwitcher />
+    </div>
     <div
         class="flex flex-col items-center min-h-screen pt-6 sm:justify-center sm:pt-0"
     >
