2d86c21810c2d96bf8f5c57ae72cfbc878cdee51
Jaksa Vlahovic <jaksa.vlahovic1@gmail.com>
2024-09-20 08:28:24 +0200

    Add heroicons

--- resources/js/Components/Sidebar/Categories.vue
+++ resources/js/Components/Sidebar/Categories.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { Link, usePage } from "@inertiajs/vue3";
 import { computed } from "vue";
+import { PlusIcon } from "@heroicons/vue/24/outline/index.js";
 
 defineProps({
     closeDrawer: Function,
@@ -12,6 +13,12 @@ const categories = computed(() => page.props.categories);
 </script>
 
 <template>
+    <div class="flex items-center justify-between">
+        <h3>Categories</h3>
+        <button>
+            <PlusIcon class="size-5 hover:text-primary focus:text-primary" />
+        </button>
+    </div>
     <ul class="menu">
         <li v-for="category in categories" :key="category.id">
             <Link
--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -4,6 +4,7 @@ import { usePage } from "@inertiajs/vue3";
 import { computed, ref } from "vue";
 import Profile from "@/Components/Sidebar/Profile.vue";
 import Categories from "@/Components/Sidebar/Categories.vue";
+import { Bars2Icon } from "@heroicons/vue/24/outline/index.js";
 
 const page = usePage();
 
@@ -32,14 +33,13 @@ function closeDrawer() {
             >
                 <!-- Page content here -->
                 <div
-                    class="flex w-full justify-end gap-x-6 pr-4 pt-4 md:pr-6 md:pt-6"
+                    class="flex w-full items-center justify-end gap-x-6 pr-4 pt-4 md:pr-6 md:pt-6"
                 >
                     <ThemeSwitcher />
-                    <label
-                        class="btn btn-primary drawer-button lg:hidden"
-                        for="my-drawer-2"
-                    >
-                        Open drawer
+                    <label class="drawer-button lg:hidden" for="my-drawer-2">
+                        <Bars2Icon
+                            class="size-6 cursor-pointer hover:text-primary focus:text-primary"
+                        />
                     </label>
                 </div>
                 <main class="w-full flex-1 px-6 py-4 sm:px-0">
@@ -56,7 +56,6 @@ function closeDrawer() {
                     <!-- Sidebar content here -->
                     <Profile :closeDrawer="closeDrawer" />
                     <div class="divider" />
-                    <div>Categories</div>
                     <Categories :closeDrawer="closeDrawer" />
                     <div class="divider" />
                     <div>Tags</div>
